a59dca977d795c56d04f0c4ac774abd11a964086
Elmael33 <mael.salet33@gmail.com>
2025-09-22 15:10:41 +0200

    [feat] implement an algorithm that check if each boxes is opened

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+    """
+    if not boxes or not isinstance(boxes, list):
+        return False
+ 
+    opened = set([0])
+    keys = [0]
+
+    while keys:
+        current_box = keys.pop()
+        for key in boxes[current_box]:
+            if key not in opened and 0 <= key < len(boxes):
+                opened.add(key)
+                keys.append(key)
+
+    return len(opened) == len(boxes)
