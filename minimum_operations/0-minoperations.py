fa83beb07dd24dbbc29ace5399afce68a01e5b96
Elmael33 <mael.salet33@gmail.com>
2025-11-21 13:58:16 +0100

    [feat] implement a script for max binary heap with level order insertion

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,12 @@
+def minOperations(n):
+	"""Calculate the fewest number of operations to get n H characters."""
+	if n < 2:
+		return 0
+	ops = 0
+	factor = 2
+	while n > 1:
+		while n % factor == 0:
+			ops += factor
+			n //= factor
+		factor += 1
+	return ops
